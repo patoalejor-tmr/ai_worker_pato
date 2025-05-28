bbbff67e94e9bf95d49b06d77440ccf9ed5a9e9e
Wonho Yun <ywh@robotis.com>
2025-05-28 09:44:57 +0900

    Deleted and Modified the udev rules's legacy
    Signed-off-by: Wonho Yun <ywh@robotis.com>

--- ffw_bringup/setup.py
+++ ffw_bringup/setup.py
@@ -20,7 +20,6 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name, ['ffw.rules']),
         ('share/' + package_name + '/launch', glob('launch/*.launch.py')),
         (os.path.join('share', package_name, 'config/ffw_bg2_follower'), glob('config/ffw_bg2_follower/*')),
         (os.path.join('share', package_name, 'config/ffw_bg2_rev2_follower'), glob('config/ffw_bg2_rev2_follower/*')),
