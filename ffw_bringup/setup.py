7a8c961838403967d039253a8954e1dd5eaefa08
Woojin Wie <wwj@robotis.com>
2025-06-13 17:17:37 +0900

    Add configuration for ffw_sg2_rev1 model

--- ffw_bringup/setup.py
+++ ffw_bringup/setup.py
@@ -27,6 +27,8 @@ setup(
          glob('config/ffw_bg2_rev3_follower/*')),
         (os.path.join('share', package_name, 'config/ffw_bg2_rev4_follower'),
          glob('config/ffw_bg2_rev4_follower/*')),
+        (os.path.join('share', package_name, 'config/ffw_sg2_rev1_follower'),
+         glob('config/ffw_sg2_rev1_follower/*')),
         (os.path.join('share', package_name, 'config/ffw_lg2_leader'),
          glob('config/ffw_lg2_leader/*')),
         (os.path.join('share', package_name, 'config/common'), glob('config/common/*')),
