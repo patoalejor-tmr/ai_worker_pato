a9a3386592bbad94a1f88729c487a820aa6b2616
Wonho Yun <ywh@robotis.com>
2025-07-28 16:14:46 +0900

    Modified EOF
    Signed-off-by: Wonho Yun <ywh@robotis.com>

--- ffw_description/launch/ffw_bg2_rev4.launch.py
+++ ffw_description/launch/ffw_bg2_rev4.launch.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# Author: Wonho Yoon, Sungho Woo
+# Author: Wonho Yun, Sungho Woo
 
 import os
 
